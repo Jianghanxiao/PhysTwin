bb4d7ebf82372630578629425ee90c03fa1963b7
Hanxiao Jiang <jhxiao@amazon.com>
2025-06-09 16:54:44 -0700

    update

--- qqtt/engine/trainer_warp.py
+++ qqtt/engine/trainer_warp.py
@@ -1079,7 +1079,7 @@ class InvPhyTrainerWarp:
         # Initialize keyboard tracking variables
         self.pressed_keys = set()  # Set to track all active keys (both physical and virtual)
         self.virtual_keys = {}     # Dictionary to track virtual keys with timestamps
-        self.virtual_key_duration = 0.2  # Virtual key press duration in seconds
+        self.virtual_key_duration = 0.03  # Virtual key press duration in seconds
         self.target_change = np.zeros((n_ctrl_parts, 3))
         
         # Start physical keyboard listener
